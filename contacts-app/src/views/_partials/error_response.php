9f67b54be411a240550e6327a55133ecaba1a74c
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-09-19 11:43:03 +0300

    Update error response message

--- contacts-app/src/views/_partials/error_response.php
+++ contacts-app/src/views/_partials/error_response.php
@@ -1,5 +1,7 @@
-<div class="error-wrapper">
-    <h4>Result: <span class="result">error</span></h4>
-    <h4>Status Code: <?= htmlentities($errorResponse->getStatusCode()) ?></h4>
-    <pre><?php print_r($errorResponse->getData()) ?></pre>
-</div>
+<?php if (isset($errorResponse)) { ?>
+    <div class="error-wrapper">
+        <h4>API returned an <span class="result">Error</span>:</h4>
+        <h4>Status Code: <?= htmlentities($errorResponse->getStatusCode()) ?></h4>
+        <pre><?php print_r($errorResponse->getData()) ?></pre>
+    </div>
+<?php } ?>
