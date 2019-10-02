f0f1f120a59c142a31b27663d304dfafd282fae2
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-10-02 11:29:38 +0300

    Remove owners inputs

--- companies-app/src/views/companies/show.php
+++ companies-app/src/views/companies/show.php
@@ -33,19 +33,7 @@ $hubSpot->companies()->getById($companyId)->getData();
                     $valueSanitized = htmlentities($field['value']);
                     ?>
                     <label for="<?= $nameSanitized ?>"><?= $labelSanitized ?></label>
-                    <?php if ($nameSanitized === 'hubspot_owner_id') { ?>
-                        <select name="<?= $nameSanitized ?>" id="<?= $nameSanitized ?>">
-                            <option value="">Not assigned</option>
-                            <?php foreach ($owners as $owner) { ?>
-                                <option
-                                        value="<?= $owner->ownerId ?>"
-                                        <?php if ($valueSanitized == $owner->ownerId) { ?>selected<?php } ?>
-                                ><?= $owner->firstName.' '.$owner->lastName ?></option>
-                            <?php } ?>
-                        </select>
-                    <?php } else { ?>
-                        <input type="text" name="<?= $nameSanitized ?>" id="<?= $nameSanitized ?>" value="<?= $valueSanitized ?>">
-                    <?php } ?>
+                    <input type="text" name="<?= $nameSanitized ?>" id="<?= $nameSanitized ?>" value="<?= $valueSanitized ?>">
                 <?php } ?>
 
                 <?php if (isset($company)) { ?>
