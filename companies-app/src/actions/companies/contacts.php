4ff7b568be9819f31333c149c9a35e2824a15217
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-10-04 11:37:58 +0300

    Update README

--- companies-app/src/actions/companies/contacts.php
+++ companies-app/src/actions/companies/contacts.php
@@ -2,22 +2,20 @@
 
 use Helpers\HubspotClientHelper;
 
-$companyId = $_GET['companyId'] ?: null;
-
 $hubSpot = HubspotClientHelper::createFactory();
 const CONTACT_TO_COMPANY_DEFINITION_ID = 1;
 
-if (isset($_POST['contactsIds'])) {
-    $contactsIds = array_keys($_POST['contactsIds']);
+function create_or_delete_contacts_associations($companyId, $contactsIds) {
+    $hubSpot = HubspotClientHelper::createFactory();
+    $redirectParams = [
+        'id' => $companyId,
+    ];
     foreach ($contactsIds as $contactId) {
         $data = [
             'fromObjectId' => $contactId,
             'toObjectId' => $companyId,
             'category' => 'HUBSPOT_DEFINED',
             'definitionId' => CONTACT_TO_COMPANY_DEFINITION_ID,
-        ];
-        $redirectParams = [
-            'id' => $companyId,
         ];
         if (isset($_POST['addToCompany'])) {
             // https://developers.hubspot.com/docs/methods/crm-associations/associate-objects
@@ -29,7 +27,17 @@ if (isset($_POST['contactsIds'])) {
             $redirectParams['contactsDeleted'] = true;
         }
     }
-    header('Location: /companies/show.php?'.http_build_query($redirectParams));
+    $redirectUrl = '/companies/show.php?'.http_build_query($redirectParams);
+    return $redirectUrl;
+}
+
+$companyId = $_GET['companyId'] ?: null;
+
+if (isset($_POST['contactsIds'])) {
+    $contactsIds = array_keys($_POST['contactsIds']);
+    $redirectUrl = create_or_delete_contacts_associations($companyId, $contactsIds);
+    header('Location: '.$redirectUrl);
+    exit();
 }
 
 $search = $_GET['search'];
