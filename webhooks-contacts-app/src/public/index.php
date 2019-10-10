6f39805c91dff526033badd8d6ce6df890e60b15
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-10-10 12:04:36 +0300

    Fix webhooks authorization

--- webhooks-contacts-app/src/public/index.php
+++ webhooks-contacts-app/src/public/index.php
@@ -1,32 +1,4 @@
 <?php
-/*
-include_once '../../vendor/autoload.php';
-
-try {
-    session_start();
-    \Repositories\EventsRepository::createTableIfNotExists();
-    $uri = parse_url($_SERVER["REQUEST_URI"])['path'];
-    switch ($uri) {
-        case '/' :
-            header('Location: /webhooks/events.php');
-            exit();
-        case '/webhooks/events.php':
-        case '/webhooks/handle.php':
-        case '/oauth/authorize.php':
-        case '/oauth/callback.php':
-            $path = __DIR__ .'/../actions'. $uri;
-            require $path;
-            exit();
-        default:
-            http_response_code(404);
-            exit();
-    }
-} catch (Throwable $t) {
-    $message = $t->getMessage();
-    include __DIR__.'/../views/error.php';
-    exit();
-}*/
-
 
 use Helpers\Oauth2Helper;
 use Repositories\EventsRepository;
@@ -43,6 +15,7 @@ try {
         case '/oauth/login.php':
         case '/oauth/authorize.php':
         case '/oauth/callback.php':
+        case '/webhooks/handle.php':
             $path = __DIR__ . '/../actions' . $uri;
             require $path;
             exit();
@@ -59,7 +32,6 @@ try {
             header('Location: /webhooks/events.php');
             exit();
         case '/webhooks/events.php':
-        case '/webhooks/handle.php':
             $path = __DIR__ . '/../actions' . $uri;
             require $path;
             exit();
@@ -72,4 +44,3 @@ try {
     include __DIR__ . '/../views/error.php';
     exit();
 }
-
