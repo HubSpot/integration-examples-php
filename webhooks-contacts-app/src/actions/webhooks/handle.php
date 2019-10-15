8f748108b4768eff9aaf88bdaf4aa61bc2b86409
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-10-15 12:22:20 +0300

    Verify HubSpot signature

--- webhooks-contacts-app/src/actions/webhooks/handle.php
+++ webhooks-contacts-app/src/actions/webhooks/handle.php
@@ -1,13 +1,25 @@
 <?php
 
 use Helpers\DBClientHelper;
+use Repositories\EventsRepository;
+
+function verify_hubspot_signature() {
+    $requestSignature = $_SERVER['HTTP_X_HUBSPOT_SIGNATURE'];
+    $requestBody = file_get_contents('php://input');
+    $clientSecret = $_ENV['HUBSPOT_CLIENT_SECRET'];
+    $requiredSignature = hash('sha256', $clientSecret.$requestBody);
+    if ($requestSignature !== $requiredSignature) {
+        header("HTTP/1.1 401 Unauthorized");
+        exit();
+    }
+}
+
+$requestBody = file_get_contents('php://input');;
+verify_hubspot_signature();
 
 $db = DBClientHelper::getClient();
-
-$requestBody = file_get_contents('php://input');
-
 $events = json_decode($requestBody, true);
 
 foreach ($events as $event) {
-    \Repositories\EventsRepository::saveEvent($event);
+    EventsRepository::saveEvent($event);
 }
