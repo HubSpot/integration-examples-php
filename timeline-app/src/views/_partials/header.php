f939605ffc312ffb2c3215396b4e5d9360767b04
ksvirkou <ksvirkou@hubspot.com>
2019-11-12 09:35:13 +0300

    çode review

--- timeline-app/src/views/_partials/header.php
+++ timeline-app/src/views/_partials/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>HubSpot PHP sample webhooks app</title>
+    <title>HubSpot PHP Sample Timeline App</title>
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.css">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/milligram/1.3.0/milligram.css">
