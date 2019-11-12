857b32084b6312e9d48355c71e135f2e259b3b16
ksvirkou <ksvirkou@hubspot.com>
2019-11-12 15:47:06 +0300

    code review

--- companies-app/src/Helpers/HubspotClientHelper.php
+++ companies-app/src/Helpers/HubspotClientHelper.php
@@ -1,32 +1,43 @@
 <?php
 
-
 namespace Helpers;
 
 use SevenShores\Hubspot\Factory;
+use SevenShores\Hubspot\Resources\OAuth2;
 use SevenShores\Hubspot\Http\Response;
 
 class HubspotClientHelper
 {
     const HTTP_OK = 200;
 
-    public static function createFactory() {
-        $accessToken = Oauth2Helper::refreshAndGetAccessToken();
-        $client = new Factory(
-            [
-                'key' => $accessToken,
-                'oauth2' => true,
-            ],
+    public static function createFactory(): Factory
+    {
+        $accessToken = OAuth2Helper::refreshAndGetAccessToken();
+        return self::create([
+            'key' => $accessToken,
+            'oauth2' => true,
+        ]);
+    }
+
+    public static function getOAuth2Resource(): OAuth2
+    {
+        return self::create()->oAuth2();
+    }
+
+    protected static function create($factoryConfig = []): Factory
+    {
+        return new Factory(
+            $factoryConfig,
             null,
             [
                 'http_errors' => false // pass any Guzzle related option to any request, e.g. throw no exceptions
             ],
             true
         );
-        return $client;
     }
 
-    public static function isResponseSuccessful(Response $response) {
+    public static function isResponseSuccessful(Response $response): bool
+    {
         return $response->getStatusCode() === self::HTTP_OK;
     }
 }
