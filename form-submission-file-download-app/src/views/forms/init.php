f12dceb0be317bcb98db00fb39068f97829a1acf
yfaktorovich <yfaktorovich@hubspot.com>
2019-11-19 23:49:56 -0500

    added more descriptions of what we are doing. Fixed typo

--- form-submission-file-download-app/src/views/forms/init.php
+++ form-submission-file-download-app/src/views/forms/init.php
@@ -6,11 +6,25 @@ include __DIR__.'/../_partials/header.php'; ?>
 <div class="row">
     <div class="column column-20"></div>
     <div class="column column-60">
+    <pre>
+        // src/actions/forms/init.php - form and properties initialization script
+           $propertyResponse = $hubSpot->contactProperties()->create([
+            'name' => $propetry,
+            'label' => $propetry,
+            'description' => 'HubSpot PHP sample Form Submission and File Download app use this field for uploading picture',
+            'groupName' => 'contactinformation',
+            'type' => 'string',
+            'formField' => true,
+            'fieldType' => 'file',
+        ]);
+        $hubSpotProperty = $propertyResponse->getData();
+        
+    </pre>
         <form method="post">
-            <h3>Initial Script</h3>
-            <p>This script create two custom properies, uploading form and webhook.</p>
+            <h3>Initialization Script - press Go button to initialize file upload form and Properties</h3>
+            <p>This script creates two custom properies, upload form and webhook.</p>
             <div>
-                <label>From Name</label>
+                <label>Form Name</label>
                 <input type="text" name="formName" value="<?php echo $formName; ?>">
             </div>
             <button type="submit">Go</button>
