cb927f1f8cb412d8fd97acab72675984e0910e9a
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-11-21 15:14:03 +0300

    Add supervisord to handle telegram updates

--- timeline-app/src/console/telegram/handleUpdates.php
+++ timeline-app/src/console/telegram/handleUpdates.php
@@ -9,17 +9,34 @@ require __DIR__.'/../../../vendor/autoload.php';
 
 use Telegram\Bot\Api;
 
-$telegram = new Api(getEnvOrException('TELEGRAM_BOT_API_KEY'));
+function check_updates() : void {
+    $maxUpdateId = Repositories\TelegramUpdatesRepository::findMaxId();
 
-$maxUpdateId = Repositories\TelegramUpdatesRepository::findMaxId();
-$updates = $telegram->getUpdates(['offset' => $maxUpdateId + 1]);
+    $telegram = new Api(getEnvOrException('TELEGRAM_BOT_API_KEY'));
+    $updates = $telegram->getUpdates(['offset' => $maxUpdateId + 1]);
 
-foreach ($updates as $update) {
-    \Repositories\TelegramUpdatesRepository::save(['id' => $update->update_id]);
+    foreach ($updates as $update) {
+        \Repositories\TelegramUpdatesRepository::save(['id' => $update->update_id]);
 
-    // send the same message back
-    $telegram->sendMessage([
-        'chat_id' => $update->getMessage()->chat->id,
-        'text' => $update->getMessage()->text,
-    ]);
+        var_dump($update);
+
+        // send the same message back
+        $telegram->sendMessage([
+            'chat_id' => $update->getMessage()->chat->id,
+            'text' => 'Will you go to SAMPLE EVENT?',
+            'reply_markup' => json_encode([
+                'inline_keyboard' => [
+                    [
+                        ['text' => 'YES', 'callback_data' => 1],
+                        ['text' => 'NO', 'callback_data' => 0],
+                    ]
+                ]
+            ]),
+        ]);
+    }
+}
+
+while (true) {
+    check_updates();
+    sleep(1);
 }
