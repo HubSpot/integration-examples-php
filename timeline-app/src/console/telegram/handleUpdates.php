31ab14f004bcb33fe593b5c249d164410322d49a
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-11-22 14:15:35 +0300

    Handle /start /events telegram commands

--- timeline-app/src/console/telegram/handleUpdates.php
+++ timeline-app/src/console/telegram/handleUpdates.php
@@ -1,39 +1,13 @@
 <?php
 
-/**
- * sample integration with telegram sdk
- */
-
 require __DIR__.'/../../../vendor/autoload.php';
 
-use Telegram\Bot\Api;
-
-function check_updates() : void {
-    $maxUpdateId = Repositories\TelegramUpdatesRepository::findMaxId();
-
-    $telegram = new Api(getEnvOrException('TELEGRAM_BOT_API_KEY'));
-    $updates = $telegram->getUpdates(['offset' => $maxUpdateId + 1]);
-
-    foreach ($updates as $update) {
-        \Repositories\TelegramUpdatesRepository::save(['id' => $update->update_id]);
-
-        // send the same message back
-        $telegram->sendMessage([
-            'chat_id' => $update->getMessage()->chat->id,
-            'text' => 'Will you go to SAMPLE EVENT?',
-            'reply_markup' => json_encode([
-                'inline_keyboard' => [
-                    [
-                        ['text' => 'YES', 'callback_data' => 1],
-                        ['text' => 'NO', 'callback_data' => 0],
-                    ]
-                ]
-            ]),
-        ]);
-    }
-}
+$telegramUpdatesHandler = new \Telegram\TelegramUpdatesHandler(
+    getEnvOrException('TELEGRAM_BOT_API_KEY'),
+    getEnvOrException('TELEGRAM_BOT_USERNAME')
+);
 
 while (true) {
-    check_updates();
+    $telegramUpdatesHandler->handle();
     sleep(1);
 }
