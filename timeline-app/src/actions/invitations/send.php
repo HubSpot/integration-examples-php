f2aaaeb3903597dce6fde3847331d7e763fd037e
Aleh Tanasiuk <atanasiuk@hubspot.com>
2019-12-24 14:37:09 +0300

    Clean up

--- timeline-app/src/actions/invitations/send.php
+++ timeline-app/src/actions/invitations/send.php
@@ -11,25 +11,30 @@ use Telegram\TelegramBot;
 TelegramBot::init();
 $hubSpot = HubspotClientHelper::createFactory();
 
+$sendInvitationAndCreateTimelineEvent = function($invitation, $contact) use ($hubSpot)
+{
+    $email = $contact->properties->email->value;
+    $chatId = UsersRepository::getTelegramChatIdByEmail($email);
+    if (!empty($chatId)) {
+        TelegramBot::sendInvitation($invitation, $chatId);
+        TimelineEventHelper::createEvent(
+            $chatId,
+            EventTypeCode::USER_INVITATION_ACTION,
+            [
+                'name' => $invitation['name'],
+                'action' => UserInvitationAction::RECEIVED,
+            ]
+        );
+    }
+};
+
 if (isset($_POST['listIds'])) {
     $invitation = InvitationsRepository::getById($_GET['id']);
     foreach ($_POST['listIds'] as $listId) {
         $contacts = $hubSpot->contactLists()->recentContacts($listId)->getData()->contacts;
         foreach ($contacts as $contact) {
             $contact = $hubSpot->contacts()->getById($contact->vid)->getData();
-            $email = $contact->properties->email->value;
-            $chatId = UsersRepository::getTelegramChatIdByEmail($email);
-            if (!empty($chatId)) {
-                TelegramBot::sendInvitation($invitation, $chatId);
-                TimelineEventHelper::createEvent(
-                    $chatId,
-                    EventTypeCode::USER_INVITATION_ACTION,
-                    [
-                        'name' => $invitation['name'],
-                        'action' => UserInvitationAction::RECEIVED,
-                    ]
-                );
-            }
+            $sendInvitationAndCreateTimelineEvent($invitation, $contact);
         }
     }
     $sent = true;
