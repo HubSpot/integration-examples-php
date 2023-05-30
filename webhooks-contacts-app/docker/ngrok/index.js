82fc10613a64d3bec4f531f8de149c4ce40fac8c
ksvirkou-hubspot <ksvirkou@hubspot.com>
2023-05-30 11:44:56 +0300

    Update ngrok

---
+++ webhooks-contacts-app/docker/ngrok/index.js
@@ -0,0 +1,38 @@
+const ngrok = require("ngrok");
+
+if (!process.env.TARGET_HOST || !process.env.TARGET_PORT) {
+  throw new Error(
+    "The following env variables are required: TARGET_HOST, TARGET_PORT"
+  );
+  process.exit(1);
+}
+
+const targetAddress = `${process.env.TARGET_HOST}:${process.env.TARGET_PORT}`;
+
+const validRegions = ["us", "eu", "au", "ap"];
+const region = process.env.NGROK_REGION
+  ? validRegions.includes(process.env.NGROK_REGION.toLowerCase())
+    ? process.env.NGROK_REGION.toLowerCase()
+    : "us"
+  : "us";
+
+
+const options = {
+  proto: 'http',
+  addr: targetAddress,
+  auth: process.env.NGROK_AUTH,
+  region: region,
+  authtoken: process.env.NGROK_TOKEN,
+  configPath: "/ngrok.yml"
+};
+
+ngrok
+  .connect(options)
+  .then(url => {
+    console.log(`The ngrok tunnel is active`);
+    console.log(`${url} ---> ${targetAddress}`);
+  })
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
